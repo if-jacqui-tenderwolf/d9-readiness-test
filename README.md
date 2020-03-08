e2a7cb59b85e3ed3872b6d81c8d754e6b97c6922
Lisa Ridley <lisa@ifsight.com>
2020-03-08 10:46:58 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Tabla Rasa 8 Test
+
+Build Status: [![Build Status](https://travis-ci.org/if-lhridley/tabrasa8-test.svg?branch=develop)](https://travis-ci.org/if-lhridley/tabrasa8-test)
---
+++ tabrasa8.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# This script clones the IF Tabrasa8 repository from the IFSight Github Repository
+# (https://github.com/IFSight/tabrasa8) and pushes it to the one located at https://github.com/if-lhridley/tabrasa8.
+# This is for build and test purposes only.  All maintenance work is completed on https://github.com/IFSight/tabrasa8
+
+
+# Clones the if-lhridley Tabrasa repo from Github
+git clone https://github.com/if-lhridley/tabrasa8.git docroot
+cd docroot
+# Adds tracking for all remote branches to the workspace
+git branch -r | grep -v '\->' | while read remote; do git branch --track "${remote#origin/}" "$remote"; done
+# Fetches and pulls all remote branchs and tags
+git fetch --all
+git pull --all
+git fetch --tags
