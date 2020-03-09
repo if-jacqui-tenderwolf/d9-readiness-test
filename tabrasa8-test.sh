57e00e9f7f1b964e5b3cd343bbad927fab3c4605
Lisa Ridley <lisa@ifsight.com>
2020-03-08 20:15:40 -0400

    Adding tests to tabrasa8-test.sh

--- tabrasa8-test.sh
+++ tabrasa8-test.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 
 # Executes the test scans and other tests
+
+docker-compose exec php7.3 /bin/sh -c "cd /var/www/html/docroot; ../bin/drush en admin_toolbar admin_toolbar_tools admin_toolbar_links_access_filter -y"
+docker-compose exec php7.3 /bin/sh -c "cd /var/www/html/docroot; drupal-check modules/contrib/admin_toolbar"
+docker-compose exec php7.3 /bin/sh -c "cd /var/www/html/docroot; ../bin/drush pm-uninstall admin_toolbar_links_access_filter admin_toolbar_tools admin_toolbar -y"
